0c90e0aef3212d738ce176a75202dee14813f957
rsc@rsc.pp.ru <rsc@rsc.pp.ru>
2023-05-03 20:58:36 +0300

    Добавьте файлы проекта.

---
+++ WpfMvvm/ViewModels/MainWindowVM.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfMvvm.ViewModels
+{
+    internal class MainWindowVM
+    {
+        public delegate void ShowWindow();
+        public event ShowWindow? ShowWindowEvent;
+        public delegate void ShowMessage(string message);
+        public event ShowMessage? ShowMessageEvent;
+
+        public ICommand ShowMessageCommand { get; }
+        public ICommand ShowWindowCommand { get; }
+
+        public MainWindowVM()
+        {
+            ShowMessageCommand = new ShowMessageCommandRealisation(this);
+            ShowWindowCommand = new ShowWindowCommandRealisation(this);
+        }
+
+        private class ShowMessageCommandRealisation : ICommand
+        {
+            private MainWindowVM mainWindowVM;
+            public ShowMessageCommandRealisation(MainWindowVM mainWindowVM)
+            {
+                this.mainWindowVM = mainWindowVM;
+            }
+
+            public event EventHandler? CanExecuteChanged;
+
+            public bool CanExecute(object? parameter) => true;
+
+            public void Execute(object? parameter) => mainWindowVM.ShowMessageEvent?.Invoke("test");
+        }
+
+        private class ShowWindowCommandRealisation : ICommand
+        {
+            private MainWindowVM mainWindowVM;
+            public ShowWindowCommandRealisation(MainWindowVM mainWindowVM)
+            {
+                this.mainWindowVM = mainWindowVM;
+            }
+
+            public event EventHandler? CanExecuteChanged;
+
+            public bool CanExecute(object? parameter) => true;
+            public void Execute(object? parameter) => mainWindowVM.ShowWindowEvent?.Invoke();
+            
+        }
+    }
+}
---
+++ WpfMvvm/Views/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfMvvm.ViewModels;
+using WpfMvvm.Views;
+
+namespace WpfMvvm
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+
+            InitializeComponent();
+            ((MainWindowVM)DataContext).ShowWindowEvent += MainWindow_ShowWindowEvent;
+            ((MainWindowVM)DataContext).ShowMessageEvent += MainWindow_ShowMessageEvent;
+        }
+
+        private void MainWindow_ShowMessageEvent(string message)
+        {
+            MessageBox.Show(message);
+        }
+
+        private void MainWindow_ShowWindowEvent()
+        {
+            (new AdditionalWindow()).ShowDialog();
+        }
+    }
+}
